059f71cb242c7e44cee0223789d74916fb669e88
CARLOS ANDRES CARRASCO NOVOA <cacarrasco3@espe.edu.ec>
2026-02-12 17:50:01 -0500

    cambios en week 1 C

---
+++ Week 1 C/Cash/cash/cash.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int change;
+    do
+    {
+        change = get_int("Change owed: ");
+    }
+    while (change < 0);
+
+    int coins = 0;
+
+    coins += change / 25;
+    change %= 25;
+
+    coins += change / 10;
+    change %= 10;
+
+    coins += change / 5;
+    change %= 5;
+
+    coins += change; // monedas de 1 centavo
+
+    printf("%i\n", coins);
+}
